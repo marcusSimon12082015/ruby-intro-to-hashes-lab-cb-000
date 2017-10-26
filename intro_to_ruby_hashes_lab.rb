472238bab54eba7e6a4f14d8434973f4b94ac110
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-26 19:57:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,13 +8,14 @@ end
 
 def monopoly
 	monopoly = {
-		:railroads => {
-			:pieces => 4,
-			:rent_in_dollars=>{
-				:one_piece_owned=>25,
-				:two_pieces_owned=>50,
-				:three_pieces_owned=>100,
-				:four_pieces_owned=>200
+		:railroads => 
+		{
+#			:pieces => 4,
+#			:rent_in_dollars=>{
+#				:one_piece_owned=>25,
+#				:two_pieces_owned=>50,
+#				:three_pieces_owned=>100,
+#				:four_pieces_owned=>200
 			},
 			:names=>
 			{
